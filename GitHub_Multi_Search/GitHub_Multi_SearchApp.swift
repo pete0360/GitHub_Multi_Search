06e1697ba0700968b7d3d6151a4851a8ef18d06c
bpeterkins28 <ben.peterkins@gmail.com>
2022-06-03 20:18:26 -0400

    Initial Commit

---
+++ GitHub_Multi_Search/GitHub_Multi_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Multi_SearchApp.swift
+//  GitHub_Multi_Search
+//
+//  Created by Ben Peterkins on 2022-06-03.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Multi_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
